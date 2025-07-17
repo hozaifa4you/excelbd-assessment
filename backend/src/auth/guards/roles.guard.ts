4d95e5bb23b89adab83732b9bd8fd44995809ca5
Hozaifa Bin Masud <hozaifabinmasud@gmail.com>
2025-07-17 23:49:50 +0600

    Roles guards

---
+++ backend/src/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { ROLE_KEY } from '../decorators/roles.decorator';
+import { Role } from 'generated/prisma';
+import { Request } from 'express';
+import { AuthRequest } from '../types/auth-user';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+   constructor(private readonly reflector: Reflector) {}
+
+   canActivate(
+      context: ExecutionContext,
+   ): boolean | Promise<boolean> | Observable<boolean> {
+      const requiredRoles = this.reflector.getAllAndOverride<
+         Role[] | undefined
+      >(ROLE_KEY, [context.getHandler(), context.getClass()]);
+
+      if (!requiredRoles) {
+         return true;
+      }
+
+      const request = context.switchToHttp().getRequest<AuthRequest>();
+
+      const userRole = request.user.role;
+      const rolesSet = new Set(requiredRoles);
+
+      return rolesSet.has(userRole);
+   }
+}
